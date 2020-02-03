7b49dfa069edbbb3fd4526b7b86da6c0549ddd51
bastien17-dev <bastien.paquier@gmail.com>
2020-02-03 16:45:08 +0100

    add visieres files

--- src/js/collection.js
+++ src/js/collection.js
@@ -1,10 +1,10 @@
-let seeButton = document.querySelectorAll(".collection__button");
-let divSpeedBuy = document.querySelectorAll(".collection__speedbuy");
+let seeButton = document.querySelectorAll('.collection__button');
+let divSpeedBuy = document.querySelectorAll('.collection__speedbuy');
 console.log(seeButton);
 console.log(divSpeedBuy);
 
 for (let i = 0; i < seeButton.length; i++) {
-  seeButton[i].addEventListener("click", function() {
-    divSpeedBuy[i].classList.toggle("is-selected");
+  seeButton[i].addEventListener('click', function() {
+    divSpeedBuy[i].classList.toggle('is-selected');
   });
 }
