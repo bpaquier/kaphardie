f7222d1acde200a5dc1842373880543d093c96d2
bastien17-dev <bastien.paquier@gmail.com>
2020-02-03 12:38:18 +0100

    add quantity fonctions

--- src/js/personnalisation.js
+++ src/js/personnalisation.js
@@ -1,3 +1,6 @@
+let $priceElt = document.querySelector('.price');
+let price = 249.99;
+
 function selectProduct(product) {
   let prevSelection = document.querySelector('.' + product + '.is-selected');
   document.querySelectorAll('.' + product).forEach(element => {
@@ -9,8 +12,45 @@ function selectProduct(product) {
   });
 }
 
+function chooseQuantity() {
+  let canClose = false;
+  let $personnalisation = document.querySelector('.personnalisation');
+  let $quantityChoosen = document.querySelector('.quantity__value');
+  let $QuantitySelection = document.querySelector('.quantity');
+  let $QuantityList = document.querySelector('.quantity__choice');
+  let $quantityValue = document.querySelectorAll('.quantity__choice--value');
+  $priceElt.innerHTML = price + ' €';
+  $QuantitySelection.addEventListener('click', function() {
+    if (!canClose) {
+      $QuantitySelection.classList.add('is-choosing');
+      $QuantityList.classList.add('is-visible');
+      setTimeout(() => {
+        canClose = true;
+      }, 10);
+    }
+  });
+  $quantityValue.forEach(element => {
+    element.addEventListener('click', function() {
+      let quantity = element.getAttribute('value');
+      $priceElt.innerHTML = price * parseInt(quantity) + ' €';
+      $quantityChoosen.innerHTML = quantity;
+      $QuantitySelection.classList.remove('is-choosing');
+      $QuantityList.classList.remove('is-visible');
+      canClose = false;
+    });
+  });
+  $personnalisation.addEventListener('click', function() {
+    if (canClose) {
+      $QuantitySelection.classList.remove('is-choosing');
+      $QuantityList.classList.remove('is-visible');
+      canClose = false;
+    }
+  });
+}
+
 selectProduct('sample--visiere');
 selectProduct('sample--tissu');
 selectProduct('sample--bouton');
 selectProduct('logos');
 selectProduct('view-picture');
+chooseQuantity();
