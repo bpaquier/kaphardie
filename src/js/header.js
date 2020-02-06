66fefc2da61f0a3b996a011df31121e9738f89de
bastien17-dev <bastien.paquier@gmail.com>
2020-02-06 17:43:28 +0100

    prevent default on clic on burger menu

--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,11 @@
-let $itemsMenu = document.querySelectorAll(".can-close");
-let $menuContent = document.querySelector(".header__menuContent");
-let $iconBurger = document.querySelector(".header__iconBurger");
-
-console.log($itemsMenu);
+let $itemsMenu = document.querySelectorAll('.can-close');
+let $menuContent = document.querySelector('.header__menuContent');
+let $iconBurger = document.querySelector('.header__iconBurger');
 
 $itemsMenu.forEach(item => {
-  item.addEventListener('click', function () {
-    $menuContent.classList.toggle("is-open");
-    $iconBurger.classList.toggle("is-open");
+  item.addEventListener('click', function(e) {
+    e.preventDefault();
+    $menuContent.classList.toggle('is-open');
+    $iconBurger.classList.toggle('is-open');
   });
 });
-
